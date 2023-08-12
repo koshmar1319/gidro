42b5ff9c6e458965409829ea5583a94bae6e2107
Kirill Komarkov <koshmar1319@gmail.com>
2023-08-12 16:09:40 +0300

    add product-page section product-card

--- js/main.js
+++ js/main.js
@@ -52,4 +52,10 @@ $(function () {
     $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
     $('.product-item__wrapper').addClass('product-item__wrapper--list');
   });
+
+  $('.rate-yo').rateYo({
+    ratedFill: '#1c62cd',
+    spacing: '7px',
+    normalFill: '#c4c4c4',
+  });
 });
