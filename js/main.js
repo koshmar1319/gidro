56a4f353be65f35181dc1302cefecfe336617ec2
Kirill Komarkov <koshmar1319@gmail.com>
2023-08-16 15:06:09 +0300

    add adaptive products and banner section

--- js/main.js
+++ js/main.js
@@ -34,6 +34,36 @@ $(function () {
       '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
     nextArrow:
       '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1301,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 3,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+        },
+      },
+    ],
   });
 
   $('.filter-style').styler();
