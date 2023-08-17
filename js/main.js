ef031f86c801740294bd8b8279eb6be4e7e2ed8b
Kirill Komarkov <koshmar1319@gmail.com>
2023-08-17 18:32:50 +0300

    add adaptive catalog page and product page

--- js/main.js
+++ js/main.js
@@ -105,4 +105,8 @@ $(function () {
     $(this).next().slideToggle();
     $(this).toggleClass('footer__topdrop--active');
   });
+
+  $('.aside__btn').on('click', function () {
+    $(this).next().slideToggle();
+  });
 });
